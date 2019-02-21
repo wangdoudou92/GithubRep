e7a0f75caa28b125da84b74540d506770af4a17a
wangdoudou92 <47837702+wangdoudou92@users.noreply.github.com>
2019-02-21 10:08:12 +0800

    Demo001.java
    第一个hello world程序

---
+++ Demo001.java
@@ -0,0 +1,23 @@
+
+public class Demo001 {
+    /**
+     * 我是文本注释
+     * 书写程序的信息介绍
+     * @param args
+     */
+    
+//    main 主方法，Java程序的主入口
+    public static void main(String[] args) {
+        // 我是单行注释
+        
+        /*
+        * 我是多行注释
+        * 可以书写多行
+        * 注释不会被程序编译和执行
+        * */
+        
+//        输出显示到控制台
+        System.out.println("HelloWorld");
+    }
+
+}
